229404b515d005101b8ced5a499b73d33d1700d4
Adam Wilson <adam@Adam-Wilsons-MacBook-Pro.local>
2009-08-09 00:31:03 +0100

    phonebook v0.1

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,13 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+// The right way to add load events. By Simon Willison
+function addLoadEvent(func) {
+  var oldonload = window.onload;
+  if (typeof window.onload != 'function') {
+    window.onload = func;
+  }
+  else {
+    window.onload = function() {
+      oldonload();
+      func();
+    }
+  }
+}
